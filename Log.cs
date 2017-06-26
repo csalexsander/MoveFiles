faac18fb45d05a9fa11925468d9f798c7103a488
csalexsander <csalexsander@gmail.com>
2017-06-26 17:44:49 -0300

    Initial commit

---
+++ Log.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveFiles
+{
+    static class Log
+    {
+        static string arquivo = @"C:\Oobj\Aplicativos\Oobj\move-thousand-files\Logs\move-thousand-files.txt";
+
+        public static void IniciandoCopia(String arquivoAmover)
+        {
+            StreamReader leitura = new StreamReader(arquivo);
+            string line = String.Empty;
+            List<string> Linhas = new List<string>();
+            bool cond = true;
+
+            while (cond)
+            {
+                line = leitura.ReadLine();
+                if (line != null)
+                {
+                    Linhas.Add(line);
+                }
+                else
+                {
+                    cond = false;
+                }
+            }
+            leitura.Close();
+            Linhas.Add("Iniciando a copia do Arquivo " + arquivoAmover);
+
+            File.WriteAllLines(arquivo, Linhas);
+        }
+        public static void FinalzandoCopia(string arquivomovido)
+        {
+            StreamReader leitura = new StreamReader(arquivo);
+            string line = String.Empty;
+            List<string> Linhas = new List<string>();
+            bool cond = true;
+
+            while (cond)
+            {
+                line = leitura.ReadLine();
+                if (line != null)
+                {
+                    Linhas.Add(line);
+                }
+                else
+                {
+                    cond = false;
+                }
+            }
+            leitura.Close();
+            Linhas.Add("Finalizada a copia do Arquivo " + arquivomovido);
+            File.WriteAllLines(arquivo, Linhas);
+        }
+        public static void ExcecaoCopia(string excecao)
+        {
+            StreamReader leitura = new StreamReader(arquivo);
+            string line = String.Empty;
+            List<string> Linhas = new List<string>();
+            bool cond = true;
+
+            while (cond)
+            {
+                line = leitura.ReadLine();
+                if (line != null)
+                {
+                    Linhas.Add(line);
+                }
+                else
+                {
+                    cond = false;
+                }
+            }
+            leitura.Close();
+            Linhas.Add(@"Copia do arquivo Não foi realizada por conta da seguinte exceção: " + excecao);
+            File.WriteAllLines(arquivo, Linhas);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretorioOrigem = @"C:\NFe\xml-exploder\entrada_BKP";
+            string diretorioDestino = @"C:\NFe\xml-exploder\entrada";
+
+          try
+            {
+                string[] files = Directory.GetFiles(diretorioOrigem, "*.xml", SearchOption.AllDirectories);
+                int restante = files.Length;
+                int ultimo = 0;
+                while (restante > 0)
+                {
+                    int valor = (restante - 3000) > 0 ? 3000 : (3000 - restante);
+                    restante -= valor;
+                    Console.WriteLine("Iniciando Copia dos Arquivos, Cerca de "+ restante + " restantes");
+                    for (int i = 0; i < valor; i++)
+                    {
+                        Log.IniciandoCopia(files[ultimo]);
+                        string destino = Path.Combine(diretorioDestino, FileName(files[ultimo]));
+                        File.Move(files[ultimo], destino);
+                        Log.FinalzandoCopia(FileName(files[ultimo]) + "PARA " + diretorioDestino);
+                        ultimo++;
+                    }
+                    Console.WriteLine("Pausa de 30 Segundos");
+                    Thread.Sleep(30000);
+                }
+
+
+                Console.WriteLine(files.Length + " arquivos processados, pressione qualquer tecla para sair");
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Log.ExcecaoCopia(ex.ToString());
+            }
+        }
+
+        public static string FileName(string arquivo)
+        {
+            string[] split = arquivo.Split(Convert.ToChar(@"\"));
+            return split[split.Length-1];
+        }
+    } 
+}
